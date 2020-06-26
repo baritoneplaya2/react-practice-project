900af5d78a0f133d73774665f687a841eb0042e5
Maria Vanessa Moreno <mariavanessa@MacBook-Pro.wtf>
2020-06-26 12:11:48 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App!!!</h1>
+        <p>This is really working</p>
+        <Person />
+        <Person />
+        <Person />
+      </div>
+    );
+    // above code gets translated into below code
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
+  }
+}
+
+export default App;
