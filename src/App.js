c7364567312dedaf86ab97e272cb1af63342e794
System Administrator <root@MacBook-Pro.wtf>
2020-06-29 10:01:53 -0400

    added state with persons info as an array  and switchNameHandler

--- src/App.js
+++ src/App.js
@@ -3,14 +3,40 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ],
+    // this value won't change when the above value is changed below
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = () => {
+    // to test that it works on the console
+    // console.log('Was clicked!');
+
+    // DO NOT DO THIS: 
+    // this.state.persons[0].name = 'Maximilian';
+
+    // right way of doing it:
+    this.setState({persons: [
+      { name: 'Maximilian', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 27 }
+    ]})
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App!!!</h1>
         <p>This is really working</p>
-        <Person />
-        <Person />
-        <Person />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name}  age={this.state.persons[1].age}  >My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name}  age={this.state.persons[2].age} />
       </div>
     );
     // above code gets translated into below code
