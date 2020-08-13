98ffc6759b1651bc537c14a29343e6e2edb808e8
Maria Vanessa Moreno <MariaVanessaMoreno@outlook.com>
2020-08-13 12:42:16 -0400

    Learning to use Higher Order Components (hoc)

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Aux from '../../../hoc/Aux';
 import classes from './Person.css';
 
 // ES6 code
@@ -7,13 +8,17 @@ class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <div className={classes.Person}>
+            <Aux>
                 <p onClick={this.props.click}>
                     I'm {this.props.name} and I am {this.props.age} years old!
-                </p>
-                <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>
+                </p>,
+                <p key="i2">{this.props.children}</p>,
+                <input
+                    key="i3"
+                    type="text"
+                    onChange={this.props.changed} value={this.props.name}
+                />
+            </Aux>
         );
     }
 };
